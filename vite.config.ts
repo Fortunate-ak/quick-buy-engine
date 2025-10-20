16d7f2ae81a2efff1624bfcc44fed4d45654cad0
Fortunate-ak <tiffanysharks17@gmail.com>
2025-10-20 13:08:41 +0200

    fixingthe error 404

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,14 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+// 💡 IMPORTANT: Replace <YOUR-REPO-NAME> with the actual name of your GitHub repository.
+const base_path = '/<YOUR-REPO-NAME>/';
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // 💡 ADD THIS LINE 💡
+  base: base_path, 
+  // ------------------
   server: {
     host: "::",
     port: 8080,
